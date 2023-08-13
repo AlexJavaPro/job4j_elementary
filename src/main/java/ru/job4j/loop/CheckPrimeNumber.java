3b0a5d7c5750f86908c9c976cf23a17e64508f27
amajo <a.majoroff2016@gmail.com>
2023-08-13 08:10:17 +0300

    5.5. Простое число (оператор прерывания break) [#171694 #436395]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,17 +2,9 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = true;
-        int count = 0;
-        for (int i = 1; i <= number; i++) {
-            if (number == 1) {
-                prime = false;
-                break;
-            }
+        boolean prime = number > 1;
+        for (int i = 2; i < number; i++) {
             if (number % i == 0) {
-               count++;
-           }
-            if (count > 2) {
                 prime = false;
                 break;
             }
